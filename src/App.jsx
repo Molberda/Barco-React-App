435aa4bc7903a6842ee17aa3c881f30f109b012b
David <davidmollera@hotmail.com>
2022-11-24 18:55:54 -0500

    changed file type from js to jsx (app.jsx) imported nav component on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Route, Router, Routes } from 'react-router-dom';
 import './App.css';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <Router>
+      <Nav/>
       <Routes>
         <Route />
       </Routes>
