78be494373ea9d92445a220f19a343cb5884dde7
David <davidmollera@hotmail.com>
2022-11-24 21:01:37 -0500

    fixed route elements in app.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { BrowserRouter as Router, Route,  Routes } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav';
 import Home from './Pages/Home';
+import Menu from './Pages/Menu';
 
 function App() {
   return (
     <Router>
       <Nav/>
       <Routes>
-        <Route to="/" element={<Home/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/menu" element={<Menu/>}/>
       </Routes>
     </Router>
   );
---
+++ src/Pages/Menu.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Menu = () => {
+    return (
+        <div>
+            iaeuhdiuaeh
+        </div>
+    );
+}
+
+export default Menu;
