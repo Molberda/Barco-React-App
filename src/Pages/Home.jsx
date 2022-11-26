dc6426d5c1267276c0a7de936be88bea17ccb5fa
David <davidmollera@hotmail.com>
2022-11-25 23:37:18 -0500

    added section landing and removed the margin from row class

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import Landing from '../components/Landing';
+import React from "react";
+import Landing from "../components/Landing";
 
 const Home = () => {
-    return (
-        <div className='container'>
-            <Landing />
-        </div>
-    );
-}
+  return (
+    <section id="landing">
+      <div className="container">
+        <Landing />
+      </div>
+    </section>
+  );
+};
 
 export default Home;
