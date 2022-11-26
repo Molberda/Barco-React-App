814056e9d88575086649a64b2d1b294f577331cf
David <davidmollera@hotmail.com>
2022-11-25 23:12:39 -0500

    created the typing wrapper and its content, using after pseudo selector to give a typing illusion

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React from "react";
 
 const Landing = () => {
-    return (
-        <div className='row'>
-            <div className="organizer">
-                <h1 className="title">BAR.CO Pola</h1>
-                <h2 className="landing__text text">EL MEJOR LUGAR PARA IR A PASAR UN BUEN RATO CON LA GENTE QUE MÁS QUIERES.</h2>
-            </div>
+  return (
+    <div className="row">
+      <div className="organizer">
+        <h1 className="title">BAR.CO Pola</h1>
+        <h2 className="landing__text text">
+          EL MEJOR LUGAR PARA IR A PASAR UN BUEN RATO CON LA GENTE QUE MÁS
+          QUIERES.
+        </h2>
+        <div class="typing__wrapper">
+          <h2 class="landing__text static__text">TENEMOS</h2>
+          <ul class="dynamic__texts">
+            <li>
+              <span class="typing__text">LA MEJOR POLA</span>
+            </li>
+            <li>
+              <span class="typing__text">LA MEJOR MÚSICA</span>
+            </li>
+            <li>
+              <span class="typing__text">LA MEJOR COMPAÑÍA</span>
+            </li>
+            <li>
+              <span class="typing__text">EL MEJOR PRECIO</span>
+            </li>
+          </ul>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Landing;
