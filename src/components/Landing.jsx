c78b73a220e2fd376ef5da8fd361db851088dbe6
David <davidmollera@hotmail.com>
2022-11-25 23:08:01 -0500

    added an organizer for landing page and a title

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Landing = () => {
     return (
         <div className='row'>
-            
+            <div className="organizer">
+                <h1 className="title">BAR.CO Pola</h1>
+            </div>
         </div>
     );
 }
