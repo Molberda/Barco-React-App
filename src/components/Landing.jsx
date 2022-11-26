a5116e73a717bfcd3a2dede15ef5b4184136e5fa
David <davidmollera@hotmail.com>
2022-11-25 23:10:43 -0500

    created css properties for .container added landing text

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -5,6 +5,7 @@ const Landing = () => {
         <div className='row'>
             <div className="organizer">
                 <h1 className="title">BAR.CO Pola</h1>
+                <h2 className="landing__text text">EL MEJOR LUGAR PARA IR A PASAR UN BUEN RATO CON LA GENTE QUE MÁS QUIERES.</h2>
             </div>
         </div>
     );
