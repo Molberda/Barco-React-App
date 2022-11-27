8c209936aa1a85672e8ea273cdc4d3181c782b47
David <davidmollera@hotmail.com>
2022-11-26 23:39:10 -0500

    created scroll component

---
+++ src/ui/Scroll.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Scroll = () => {
+    return (
+        <a href="#contactanos">
+        <div class="scroll__icon click">
+          <div class="scroll__dot"></div>
+        </div>
+      </a>
+    );
+}
+
+export default Scroll;
