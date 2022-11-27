3fa07849ebe4a173f8ecb320c3673d46fddf46d2
David <davidmollera@hotmail.com>
2022-11-27 00:28:53 -0500

    created an organizer for acerca component

--- src/components/Acerca.jsx
+++ src/components/Acerca.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import acercaImg from '../assets/barco_noche.jpeg'
+import React from "react";
+import acercaImg from "../assets/barco_noche.jpeg";
 
 const Acerca = () => {
-    return (
-       <section id="acerca">
-        <div className="container">
-            <div className="row">
-                <figure className="acerca__img--wrapper">
-                    <img src={acercaImg} alt="" className="acerca__img" />
-                </figure>
-                <h1 className="title"></h1>
-            </div>
+  return (
+    <section id="acerca">
+      <div className="container">
+        <div className="row">
+          <figure className="acerca__img--wrapper">
+            <img src={acercaImg} alt="" className="acerca__img" />
+          </figure>
+          <div className="organizer">
+            <h1 className="title">¿QUIENES SOMOS?</h1>
+          </div>
         </div>
-       </section>
-    );
-}
+      </div>
+    </section>
+  );
+};
 
 export default Acerca;
