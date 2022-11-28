e0771cf83d598f4f074e065841c8cc141d7aa013
David <davidmollera@hotmail.com>
2022-11-28 18:27:00 -0500

    placed the scroll component as a child inside the row class in acerca component

--- src/components/Acerca.jsx
+++ src/components/Acerca.jsx
@@ -12,11 +12,16 @@ const Acerca = () => {
           </figure>
           <div className="organizer acerca__organizer">
             <h1 className="title acerca__title">¿QUIENES SOMOS?</h1>
-            <h2 className="text acerca__text">Bar.Co es un bar que se caracteriza por ser la única empresa en apostarle a ser low cost en su segmento, a diferencia de las tiendas de barrio tiene un ambiente moderno y aspiracional conservado los mejores precios.</h2>
+            <h2 className="text acerca__text">
+              Bar.Co es un bar que se caracteriza por ser la única empresa en
+              apostarle a ser low cost en su segmento, a diferencia de las
+              tiendas de barrio tiene un ambiente moderno y aspiracional
+              conservado los mejores precios.
+            </h2>
           </div>
+          <Scroll link="#contactanos" />
         </div>
       </div>
-      <Scroll link="#contactanos"/>
     </section>
   );
 };
