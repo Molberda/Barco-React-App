f3ca20aa35f53fc6af9376f9d07efa7347094121
David <davidmollera@hotmail.com>
2022-11-28 21:25:31 -0500

    created list items and added a row__column class to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,17 +3,31 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <a href="#landing" className="logo__link">
-        <figure className="footer__logo--wrapper">
-          <img src="" alt="" className="footer__logo" />
-        </figure>
-      </a>
-      <div className="footer__organizer">
-        <ul className="footer__link--list">
-          <li className="footer__list--item">
-            <a href="" className="footer__link"></a>
-          </li>
-        </ul>
+      <div className="row__column">
+        <a href="#landing" className="logo__link">
+          <figure className="footer__logo--wrapper">
+            <img src="" alt="" className="footer__logo" />
+          </figure>
+        </a>
+        <div className="footer__organizer">
+          <ul className="footer__link--list">
+            <li className="footer__list--item">
+              <a href="#acerca" className="footer__link">
+                Acerca de Nosotros
+              </a>
+            </li>
+            <li className="footer__list--item">
+              <a href="#contactanos" className="footer__link">
+                Contáctanos
+              </a>
+            </li>
+            <li className="footer__list--item">
+              <a href="/menu" className="footer__link">
+                Menú
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
