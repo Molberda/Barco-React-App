ae3d0fe5bdc96c258a061ebcb346d293d93f6e2f
David <davidmollera@hotmail.com>
2022-11-28 21:41:32 -0500

    added css properties to all items in footer component, also added footer link hover effects

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,16 +15,19 @@ const Footer = () => {
               <a href="#acerca" className="footer__link">
                 Acerca de Nosotros
               </a>
+              <div className="footer__link-hover-effect"></div>
             </li>
             <li className="footer__list--item">
               <a href="#contactanos" className="footer__link">
                 Contáctanos
               </a>
+              <div className="footer__link-hover-effect"></div>
             </li>
             <li className="footer__list--item">
               <a href="/menu" className="footer__link">
                 Menú
               </a>
+              <div className="footer__link-hover-effect"></div>
             </li>
           </ul>
         </div>
