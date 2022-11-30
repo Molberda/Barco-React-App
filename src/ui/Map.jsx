f431e30b5452f5f419f0542e13ace668e482d418
David <davidmollera@hotmail.com>
2022-11-29 21:05:46 -0500

    imported the map iframe into the contactanos component

--- src/ui/Map.jsx
+++ src/ui/Map.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Map = () => {
     return (
-        <figure className="map__wrapper">
-            
-        </figure>
+            <iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=calle 99 numero 9a - 29&amp;t=&amp;z=18&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+        // <figure className="map__wrapper">
+        // </figure>
     );
 }
 
