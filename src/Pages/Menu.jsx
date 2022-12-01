0ec0edf02ea364532eb3440e8a385c73cfffc6f1
David <davidmollera@hotmail.com>
2022-11-30 23:41:20 -0500

    extracted the html from menu component into the menuitem component

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -6,13 +6,7 @@ const Menu = () => {
       <div className="row__column">
         <h1 className="title">Menu Items</h1>
         <div className="menu__organizer">
-            <div className="menu__item--wrapper">
-                <figure className="item__img--wrapper">
-                    <img src="" alt="" className="item__img" />
-                </figure>
-                <h3 className="item__name">Pola</h3>
-                <h3 className="item__price">$10.00</h3>
-            </div>
+            
         </div>
       </div>
     </div>
--- src/ui/Menuitem.jsx
+++ src/ui/Menuitem.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Menuitem = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  return (
+    <div className="menu__item--wrapper">
+      <figure className="item__img--wrapper">
+        <img src="" alt="" className="item__img" />
+      </figure>
+      <h3 className="item__name">Pola</h3>
+      <h3 className="item__price">$10.00</h3>
+    </div>
+  );
+};
 
 export default Menuitem;
