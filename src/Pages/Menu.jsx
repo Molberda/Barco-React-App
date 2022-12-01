11ea7d57c560e98e4ebf8030d55de03fb9dee791
David <davidmollera@hotmail.com>
2022-11-30 23:26:58 -0500

    added a column row class to the menu page

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -3,8 +3,9 @@ import React from "react";
 const Menu = () => {
   return (
     <div className="landing__container container">
-      <div className="row">
+      <div className="row__column">
         <h1 className="title">Menu Items</h1>
+        <div className="organizer title"></div>
       </div>
     </div>
   );
