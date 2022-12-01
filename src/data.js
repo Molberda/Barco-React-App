a86ae2152d65e239dde4d106c0f027807926c8cf
David <davidmollera@hotmail.com>
2022-12-01 18:25:44 -0500

    created the corona item to use later in the react app

--- src/data.js
+++ src/data.js
@@ -4,5 +4,11 @@ export const items = [
         name: "Stella Artois",
         url: "",
         price: 7.5
-    }
+    },
+    {
+        id: 2,
+        name: "Corona",
+        url: "",
+        price: 7.5
+    },
 ]
