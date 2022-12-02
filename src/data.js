8e5c9e12d547fd25a5d6d97649dfb533b5557b06
David <davidmollera@hotmail.com>
2022-12-01 21:46:09 -0500

    created a type property for each item

--- src/data.js
+++ src/data.js
@@ -2,48 +2,56 @@ export const items = [
     {
         id: 1,
         name: "Stella Artois",
+        type: "bebida",
         url: "",
         price: 7.5
     },
     {
         id: 2,
         name: "Corona",
+        type: "bebida",
         url: "",
         price: 7.5
     },
     {
         id: 3,
         name: "Poker",
+        type: "bebida",
         url: "",
         price: 4
     },
     {
         id: 4,
         name: "Club Dorada",
+        type: "bebida",
         url: "",
         price: 4
     },
     {
         id: 5,
         name: "Aguila",
+        type: "bebida",
         url: "",
         price: 4
     },
     {
         id: 6,
         name: "Aguila Lite",
+        type: "bebida",
         url: "",
         price: 4
     },
     {
         id: 7,
         name: "Nectar Verde",
+        type: "bebida",
         url: "",
         price: 60
     },
     {
         id: 8,
         name: "Antioqueño Azul",
+        type: "bebida",
         url: "",
         price: 80
     },
