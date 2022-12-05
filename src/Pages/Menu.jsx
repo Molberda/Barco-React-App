561fc734d7a6d69952ae8fc3c97c55a30c5cf69b
David <davidmollera@hotmail.com>
2022-12-04 19:44:10 -0500

    added html code for sorting the menuitems

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -6,6 +6,12 @@ const Menu = ({ items }) => {
     <div className="landing__container container">
       <div className="row__column">
         <h1 className="title">Menú</h1>
+        <div className="filter--group">
+          <select id="filter">
+            <option value="HIGH_TO_LOW">Precio, Mayor a Menor</option>
+            <option value="LOW_TO_HIGH">Precio, Menor a Mayor</option>
+          </select>
+        </div>
         <div className="menu__organizer">
           {items.map((item) => (
             <Menuitem item={item} key={item.id}/>
