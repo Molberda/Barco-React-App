bd646ea094931683029b356974b39485b0d5bdea
David <davidmollera@hotmail.com>
2022-12-06 17:55:39 -0500

    created the html code for the skeleton component

--- src/ui/Itemskeleton.jsx
+++ src/ui/Itemskeleton.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Itemskeleton = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  return (
+    <div className="menu__item-skeleton-wrapper">
+      <span className="item__img-skeleton-wrapper"></span>
+      <span className="item__name--skeleton"></span>
+      <span className="item__price--skeleton"></span>
+    </div>
+  );
+};
 
 export default Itemskeleton;
