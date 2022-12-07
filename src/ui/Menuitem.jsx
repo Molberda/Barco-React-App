56489d993f7c9ce21f5b92135aa6146543b07e49
David <davidmollera@hotmail.com>
2022-12-06 20:36:59 -0500

    created a ternary operator to display the items if the img has loaded, otherwise display a skeleton loading state** missing yet

--- src/ui/Menuitem.jsx
+++ src/ui/Menuitem.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Menuitem = ({ item }) => {
+  const [img, setImg] = useState();
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = item.url;
+
+    image.onload = () => {
+      setTimeout(() => {
+        setImg(image);
+      }, 800);
+    }
+  });
 
   return (
     <div className="menu__item--wrapper">
-      <figure className="item__img--wrapper" onClick={() => alert("¡Esta funcion no esta incluida aun!")}>
-        <img src={item.url} alt="" className="item__img" />
-      </figure>
-      <h3 className="item__name">{item.name}</h3>
-      <h3 className="item__price">${item.price.toFixed(3)}</h3>
+      {img ? (
+        <>
+          <figure
+            className="item__img--wrapper"
+            onClick={() => alert("¡Esta funcion no esta incluida aun!")}
+          >
+            <img src={img.src} alt="" className="item__img" />
+          </figure>
+          <h3 className="item__name">{item.name}</h3>
+          <h3 className="item__price">${item.price.toFixed(3)}</h3>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
