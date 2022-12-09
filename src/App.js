d612e1aff3b1ee4f956ec1495d11948d7e7eb5ec
David <davidmollera@hotmail.com>
2022-12-08 21:08:47 -0500

    removed Nav component from the general app.js and imported and imported and used it in the home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
-import Nav from "./components/Nav";
 import Home from "./Pages/Home";
 import Menu from "./Pages/Menu";
 import { items } from "./data.js"
@@ -10,7 +9,6 @@ function App() {
   return (
     <Router>
       <div className="app">
-        <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/menu" element={<Menu items={ items } />} />
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Acerca from "../components/Acerca";
 import Contactanos from "../components/Contactanos";
 import Landing from "../components/Landing";
+import Nav from "../components/Nav";
 
 const Home = () => {
   return (
     <div>
+      <Nav/>
       <Landing />
       <Acerca />
       <Contactanos />
