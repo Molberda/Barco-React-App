d473e748931b07c15dcecbc781012bad2151e2ce
David <davidmollera@hotmail.com>
2022-12-08 21:22:46 -0500

    passed the second pair of props (name2 and link2) to the nav comopnent on the home Page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,7 +7,7 @@ import Nav from "../components/Nav";
 const Home = () => {
   return (
     <div>
-      <Nav link1="#acerca" name1="Acerca de Nosotros" />
+      <Nav link1="#acerca" name1="Acerca de Nosotros" link2="#contactanos" name2="Contáctanos" />
       <Landing />
       <Acerca />
       <Contactanos />
