18df8efd531cf567b1afad99707659bba3cc4e50
David <davidmollera@hotmail.com>
2022-12-16 21:34:31 -0500

    updated prices for items

--- src/data.js
+++ src/data.js
@@ -4,56 +4,56 @@ export const items = [
         name: "Stella Artois",
         type: "bebida alchoholica",
         url: "https://koyotebarbacoa.com/wp-content/uploads/2020/04/stella-artois-01.png",
-        price: 7.5
+        price: 8.5
     },
     {
         id: 2,
         name: "Corona",
         type: "bebida alchoholica",
         url: "https://copservir.vtexassets.com/arquivos/ids/765087-800-auto?v=637950273978830000&width=800&height=auto&aspect=true",
-        price: 7.5
+        price: 8.5
     },
     {
         id: 3,
         name: "Poker",
         type: "bebida alchoholica",
         url: "https://domicilios24siete.com/wp-content/uploads/2018/06/poker-x400.png",
-        price: 4
+        price: 5
     },
     {
         id: 4,
         name: "Club Dorada",
         type: "bebida alchoholica",
         url: "https://cdn10.totalcode.net/gastronomy/product-zoom/es/cerveza-club-colombia-dorada-330ml-bt-1.webp",
-        price: 4
+        price: 5
     },
     {
         id: 5,
         name: "Aguila",
         type: "bebida alchoholica",
         url: "https://s.cornershopapp.com/product-images/1080802.jpg?versionId=ofJWRoEtSoBDaPasaBuEmAAAArcmtO0e",
-        price: 4
+        price: 5
     },
     {
         id: 6,
         name: "Aguila Light",
         type: "bebida alchoholica",
         url: "https://koyotebarbacoa.com/wp-content/uploads/2020/04/aguila-ligth-01.png",
-        price: 4
+        price: 5
     },
     {
         id: 7,
         name: "Nectar Verde 750ml",
         type: "bebida alchoholica",
         url: "https://jumbocolombiaio.vtexassets.com/arquivos/ids/186970/7702404005041.jpg?v=637813983688800000",
-        price: 60
+        price: 80
     },
     {
         id: 8,
         name: "Antioqueño Azul 750ml",
         type: "bebida alchoholica",
         url: "https://jumbocolombiaio.vtexassets.com/arquivos/ids/186974/7702049000531.jpg?v=637813983705430000",
-        price: 80
+        price: 100
     },
     {
         id: 9,
