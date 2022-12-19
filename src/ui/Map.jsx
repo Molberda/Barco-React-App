16f21e6dbedbd5aa3d0c9135233f4c403df3be84
David <davidmollera@hotmail.com>
2022-12-19 12:53:49 -0500

    added the required title property to the iframe tag in map compnent

--- src/ui/Map.jsx
+++ src/ui/Map.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Map = () => {
     return (
         <figure className="map__wrapper">
-            <iframe className="gmap_iframe" style={{width: "100%", height: "100%"}} src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=calle 99 numero 9A-29, Barco Pola&amp;t=&amp;z=18&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+            <iframe title='map' className="gmap_iframe" style={{width: "100%", height: "100%"}} src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=calle 99 numero 9A-29, Barco Pola&amp;t=&amp;z=18&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
         </figure>
     );
 }
